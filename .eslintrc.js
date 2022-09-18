d2b4a1d3a0ba497c55161bff6388d5550b8c75ed
ltbyce <ltbyce@163.com>
2022-09-18 11:01:26 +0800

    chore: add eslintrc & prettierrc

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const path = require('path');
+
+const prettierOptions = JSON.parse(
+  fs.readFileSync(path.resolve(__dirname, '.prettierrc'), 'utf8'),
+);
+
+module.exports = {
+  extends: ['react-app', 'prettier', 'prettier/react', 'react-app/jest'],
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': ['error', prettierOptions],
+  },
+  overrides: [
+    {
+      files: ['**/*.ts?(x)'],
+      rules: { 'prettier/prettier': ['warn', prettierOptions] },
+    },
+    {
+      files: ['**/*.stories.*'],
+      rules: {
+        'import/no-anonymous-default-export': 'off',
+      },
+    },
+  ],
+};
